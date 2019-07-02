552f306475ec5bc17780505b6cef9c4eeaea0982
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-07-02 13:16:15 +0900

    グループ編集機能の実装

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,7 @@ module ChatSpace
       g.javascripts true
       g.helper true
       g.test_framework false
-    end # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    end 
+    config.i18n.default_locale = :ja
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
-  get 'messages/new' => 'messages#new'
-
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :messages, only: [:index, :create]
+  end
 end
