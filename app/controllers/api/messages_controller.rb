021f38af4e0b0112cebe65163f7e18472c126ed2
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-07-17 14:47:23 +0900

    update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Messages.where(params[:user_id]).where('id > ?', params[:id])
+    @messages = Messages.find(params[:user_id]).where('id > ?', params[:id])
   end
 end
