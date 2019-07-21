e99dcfa7d17dad602eb06ad930977123fb58b50b
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-07-21 21:02:00 +0900

    updata 日本時間表記に変更

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
+  class Application < Rails::Application
+  config.time_zone = 'Tokyo'
+  end
+  
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
